4d70a6b981e9d6ff0c730c1a11934fea6e3e887c
Anastasia <nastia.iv.91@gmail.com>
2020-11-07 20:16:29 -0800

    readme file added, comments added

--- README.md
+++ README.md
@@ -1,1 +1,44 @@
 # Weather-Application
+
+## Description 
+
+The application needs to be created to check the weather information all around the world, such as temperature, humidity, UV index and same parameters for the next five days. The application has to save the search history and present the last requested city's weather.
+
+## Table of Contents
+
+* [Installation](#installation)
+* [Usage](#usage)
+* [Links](#links)
+* [Credits](#credits)
+
+## Installation
+
+To complete this task we went to the openweathermap.org and requested an API key to use it to get the daily weather and a 5-day forecast. Using the key and the documentation on creating custom URLs and using the `jQuery ajax()` method, we were able to make a several API calls to get the required information. <br>
+Also various libraries were installed, such as jQuery, Moment.js and Bootstrap to ensure the best functionality and implementation of the appropriate styles. Google Chrome Dev Tools were used to test the application and to make sure that everything is working properly. The file structure contains index, style and script files, written in HTML, CSS and JavaScript languages and with the description comments. 
+
+## Usage
+
+When the user launches the application, he is presented with an input field and a search button to find the weather for the city of his choice. Then the screen is populated with the temperature, humidity, wind strength and the UV index section, containing the colors green, yellow, orange and red, specifying the severity of the condition. And there's also an icon, representing the current weather (cloudy, rainy or sunny).
+Down below, there's a container with a 5 day forecast for this city, also containing the same parameters. 
+<br>
+The searched city is added to the local storage, thus when the user will reopen the browser window, he will be presented with the today's forecast and the forecast for the next 5 days. Beside's that, every city the user has searched for will be presented to him on the screen in the list, underneath the search field.
+<br><br>
+Attached below the screenshots of the applicaton:<br>
+
+![User's interface](Screenshot.png)
+
+<br>
+
+## Links
+
+[Day Planner App](https://anaiva27.github.io/Weather-Application/) <br>
+[GitHub repository](https://github.com/anaiva27/Weather-Application)<br>
+Libraries:<br>
+[jQuery](https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js) <br>
+[Moment.js](https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/moment.min.js) <br>
+
+## Credits
+
+Useful articles about Javascript functions, statements and loops, local storage:<br>
+[jQuery API](https://api.jquery.com/) <br>
+[Moment.js tutorial](https://www.sitepoint.com/managing-dates-times-using-moment-js/) <br>
--- script.js
+++ script.js
@@ -2,73 +2,63 @@ $(document).ready(function() {
 
    // search button event
 $("#searchBtn").on("click", function () {
-    weather();
-    save();
+    var newData = $("#searchBox").val();
+    weather(newData);
+    save(newData);
+});
+// history button event
+$("#history").on("click",".historyBtn", function(event) {
+    var cityInput = $(event.target).text();
+    weather(cityInput);
+    //save(newData);
 });
 
-// var emptyArray = [];
-// var historyArr = JSON.parse(localStorage.getItem("data"));
-// if (!historyArr){
-//     localStorage.setItem("data", JSON.stringify(emptyArr));
-//     historyArr = emptyArr;
-// } else {
-//     weather ();
-// }
+//get the last item in array and run weather
+var lastCityInput = JSON.parse(localStorage.getItem("data"));
+var lastCity = (lastCityInput[lastCityInput.length-1]);
+weather(lastCity);
 
-
-// $(document).on("click",".list-group-item btn btn-light text-left", function(){
-    $("#historyBtn").on("click", function () {
-    console.log("help");
-    console.log($(this).text());
-    var cityInput = $("#historyBtn").val();
-    weather();
-})
- 
-    function save() {
-    var newData = $("#searchBox").val();
-
-        // if there's nothing saved then save an empty array
+    // save search history to local storage
+    function save(newData) {
         if (localStorage.getItem("data") == null) {
             localStorage.setItem("data", "[]");
         }
         // get old data and add it to the new data
         var oldData = JSON.parse(localStorage.getItem("data"));
-        // console.log(oldData);
         oldData.push(newData);
         localStorage.setItem("data", JSON.stringify(oldData));
         var savedData = localStorage.getItem("data");
-        // console.log(savedData);
-
+        // bring  data back on the screen
         if (savedData != null) {
             var parseData = JSON.parse(savedData);
             var history = $("#history");
             history.empty();
             for (i = 0; i < parseData.length; i++) {
-                var output = $("<button>").attr("type", "button").attr("id", "historyBtn").attr("class", "list-group-item btn btn-light text-left").text(parseData[i]);
+                var output = $("<button>").attr("type", "button").attr("class", "historyBtn list-group-item text-left").text(parseData[i]);
                 history.append(output);
             }
         }
     }
 
-    function weather(){ 
-    var cityInput = $("#searchBox").val();
+    // weather forecast function
+    function weather(cityInput){ 
     var currentWeather = "https://api.openweathermap.org/data/2.5/weather?q=" +
         cityInput + "&units=imperial&APPID=7455a546f39f9c232db77780672611f7";
     var date = moment().format("MM/DD/YYYY");
-    // today weather call
+    // today's weather call
     $.ajax({
         url: currentWeather,
         method: "GET",
     }).then(function (response) {
-        // console.log(response);
+        $("#currentTemp").text("Temperture: " + response.main.temp + " F");
+        $("#currentHumidity").text("Humidity: " + response.main.humidity + " %");
+        $("#currentWind").text("Wind: " + response.wind.speed + " mph");
+        // add weather icon for today's weather
         var imgIcon = "https://api.openweathermap.org/img/w/" + response.weather[0].icon + ".png";
         var todayIcon = $("<img>").attr("src", imgIcon);
         var city = $("#cityInput").text(cityInput + " (" + date + ") ");
         city.append(todayIcon);
-        $("#currentTemp").text("Temperture: " + response.main.temp + " F");
-        $("#currentHumidity").text("Humidity: " + response.main.humidity + " %");
-        $("#currentWind").text("Wind: " + response.wind.speed + " mph");
-        // lat, lon, uv call
+        // get latitude and longitude to make a call to get UV index
         var latitude = response.coord.lat;
         var longitude = response.coord.lon;
         var uvUrl = "https://api.openweathermap.org/data/2.5/uvi?lat=" + latitude + "&lon=" + longitude + "&appid=7455a546f39f9c232db77780672611f7";
@@ -76,6 +66,7 @@ $("#searchBtn").on("click", function () {
             url: uvUrl,
             method: "GET",
         }).then(function (responseUV) {
+            // add UV color classes to indicate weather condition
             if (responseUV.value <= 2) {
                 $("#uv").text(responseUV.value).addClass("green");
             } else if (responseUV.value <= 5) {
@@ -96,16 +87,18 @@ $("#searchBtn").on("click", function () {
             method: "GET",
         }).then(function (response5) {
             $("#fiveDay").empty();
+            // loop through daily weather array, pick days from 2nd to 6th, add info and append to the card
             for (i = 1; i < 6; i++) {
-                var fiveDayIcon = "https://api.openweathermap.org/img/w/" + response5.daily[i].weather[0].icon + ".png";
-                var card = $("<div>").attr("class", "card col bg-primary m-2 pad5");
-                var cardText = $("<div>").attr("class", "card-body");
-                cardText.append($("<p>").attr("class", "text-white").text(moment().add(i, "days").format("L")));
-                cardText.append($("<img>").attr("src", fiveDayIcon));
-                cardText.append($("<p>").attr("class", "text-white").text("Temp: " + response5.daily[i].temp.day + " F"));
-                cardText.append($("<p>").attr("class", "text-white").text("Humidity: " + response5.daily[i].humidity + " %"));
-                card.append(cardText);
-                $("#fiveDay").append(card);
+                    // dinamically create div elements inside the card
+                    var card = $("<div>").attr("class", "card col bg-primary m-2 pad5");
+                    var cardText = $("<div>").attr("class", "card-body");
+                    var fiveDayIcon = "https://api.openweathermap.org/img/w/" + response5.daily[i].weather[0].icon + ".png";
+                    var fiveDate = ($("<p>").attr("class", "text-white").text(moment().add(i, "days").format("L")));
+                    var fiveTemp = ($("<p>").attr("class", "text-white").text("Temp: " + response5.daily[i].temp.day + " F"));
+                    var fiveHumid = ($("<p>").attr("class", "text-white").text("Humidity: " + response5.daily[i].humidity + " %"));
+                    cardText.append(fiveDate).append($("<img>").attr("src", fiveDayIcon)).append(fiveTemp).append(fiveHumid);;
+                    card.append(cardText);
+                    $("#fiveDay").append(card);
             }
         })
     })}
